3ac49e26033bef6049638b9139b117ce29132620
T-Huwa <thomhuwa066@gmail.com>
2025-08-08 06:35:23 +0200

    removed port

--- python_backend/app.py
+++ python_backend/app.py
@@ -504,4 +504,4 @@ if __name__ == '__main__':
     print("  GET  /model-info - Model information")
     print("=" * 60)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
